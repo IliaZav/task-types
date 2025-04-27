9958cd68458c51e345dac46e638314deca91fd68
🅒🅔🅛🅛🅢 <154609769+IliaZav@users.noreply.github.com>
2025-04-27 18:23:44 +0300

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,16 +1,36 @@
-export function getPersons(): Array<{
-    name: string;
-    age: number;
-    gender: 'male' | 'female';
-}>;
+// src/index.d.ts
 
-export function personToString(
-    person:
-        | { name: string; age: number; gender: 'male' | 'female' }
-        | {
-              name: string;
-              age: number;
-              gender: 'male' | 'female';
-              company: string;
-          },
-): string;
+export type Gender = 'male' | 'female';
+
+export interface Person {
+  name: string;
+  age: number;
+  gender: Gender;
+}
+
+export interface Employee extends Person {
+  company: string;
+}
+
+/**
+ * Возвращает массив объектов с полями:
+ * - name: string
+ * - age: number
+ * - gender: 'male' | 'female'
+ */
+export function getPersons(): Person[];
+
+/**
+ * Преобразует в строку объекты типа Person или Employee.
+ * Person:
+ * - name: string
+ * - age: number
+ * - gender: 'male' | 'female'
+ *
+ * Employee:
+ * - name: string
+ * - age: number
+ * - gender: 'male' | 'female'
+ * - company: string
+ */
+export function personToString(person: Person | Employee): string;
